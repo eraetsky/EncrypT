0012b290c9fc401afe4f12abb2dfbe3f34d2c4ca
oleksiypalamarchuck@gmail.com <oleksiypalamarchuck@gmail.com>
2023-12-06 16:31:54 +0200

    Переніс оголошення паблік полів

--- include/message/ChatMessage.hpp
+++ include/message/ChatMessage.hpp
@@ -17,20 +17,22 @@
 
 class ChatMessage
 {
+public:
+	static constexpr std::size_t header_length = 4;
+	static constexpr std::size_t max_body_length = 512;
+
 private:
 	char data_[header_length + max_body_length];
 	std::size_t body_length_;
 
 public:
-	static constexpr std::size_t header_length = 4;
-	static constexpr std::size_t max_body_length = 512;
 	ChatMessage();
-	ChatMessege(std::size_t body_length);
-	const char* data() const;
-	char* data();
+	ChatMessage(std::size_t body_length);
+	const char *data() const;
+	char *data();
 	std::size_t length() const;
-	const char* body() const;
-	char* body();
+	const char *body() const;
+	char *body();
 	std::size_t body_length() const;
 	void body_length(std::size_t new_length);
 	bool decode_header();
