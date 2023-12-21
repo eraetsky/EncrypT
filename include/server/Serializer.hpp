e06381209e307b7f6d041c578dfebda83845f562
yevhenii.rayetsky <yevhenii.rayetsky@chisw.com>
2023-12-21 10:01:08 +0200

    server: add example of serialization/deserialization

--- include/server/Serializer.hpp
+++ include/server/Serializer.hpp
@@ -7,8 +7,20 @@ template <typename T>
 class DataSerializer
 {
 public:
-    static std::string Serialize(const T &data);
-    static T Deserialize(const std::string &serializedData);
+    static std::string Serialize(const T &data)
+    {
+        std::stringstream ss;
+        ss << data;
+        return ss.str();
+    }
+
+    static T Deserialize(const std::string &serializedData)
+    {
+        T data;
+        std::stringstream ss(serializedData);
+        ss >> data;
+        return data;
+    }
 };
 
 // Partial specialization for std::vector
@@ -16,8 +28,27 @@ template <typename T>
 class DataSerializer<std::vector<T>>
 {
 public:
-    static std::string Serialize(const std::vector<T> &data);
-    static std::vector<T> Deserialize(const std::string &serializedData);
+    static std::string Serialize(const std::vector<T> &data)
+    {
+        std::stringstream ss;
+        for (const auto &item : data)
+        {
+            ss << DataSerializer<T>::Serialize(item) << " ";
+        }
+        return ss.str();
+    }
+
+    static std::vector<T> Deserialize(const std::string &serializedData)
+    {
+        std::vector<T> data;
+        std::stringstream ss(serializedData);
+        T item;
+        while (ss >> item)
+        {
+            data.push_back(item);
+        }
+        return data;
+    }
 };
 
 // Full specialization for std::string
@@ -25,6 +56,13 @@ template <>
 class DataSerializer<std::string>
 {
 public:
-    static std::string Serialize(const std::string &data);
-    static std::string Deserialize(const std::string &serializedData);
+    static std::string Serialize(const std::string &data)
+    {
+        return data;
+    }
+
+    static std::string Deserialize(const std::string &serializedData)
+    {
+        return serializedData;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "Serializer.hpp"
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-   std::cout << "Hello, EncrypT!\n";
-   std::cout << "EncrypT project started!\n";
-   std::cout << "Lets go!\n";
+   int num = 42;
+   std::string serializedNum = DataSerializer<int>::Serialize(num);
+   int deserializedNum = DataSerializer<int>::Deserialize(serializedNum);
+   std::cout << "Serialized num: " << serializedNum << std::endl;
+   std::cout << "Deserialized num: " << deserializedNum << std::endl;
+
+   std::vector<int> vec = {10, 20, 30, 40, 50};
+   std::string serializedVec = DataSerializer<std::vector<int>>::Serialize(vec);
+   std::vector<int> deserializedVec = DataSerializer<std::vector<int>>::Deserialize(serializedVec);
+   std::cout << "Serialized vector: " << serializedVec << std::endl;
+   std::cout << "Deserialized vector: ";
+   for (const auto &item : deserializedVec)
+   {
+      std::cout << item << " ";
+   }
+   std::cout << std::endl;
+
+   std::string text = "Hello, EncrypT!";
+   std::string serializedText = DataSerializer<std::string>::Serialize(text);
+   std::string deserializedText = DataSerializer<std::string>::Deserialize(serializedText);
+   std::cout << "Serialized text: " << serializedText << std::endl;
+   std::cout << "Deserialized text: " << deserializedText << std::endl;
 }
